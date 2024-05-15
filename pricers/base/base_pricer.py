72a95d67781aa6e77383b35fb60bf0292374c01a
George Pittock <gpittock4@gmail.com>
2024-05-15 15:38:40 +0100

    use one for loop for creating options as more efficient

--- pricers/base/base_pricer.py
+++ pricers/base/base_pricer.py
@@ -5,16 +5,22 @@ import numpy as np
 
 class BasePricer(ABC):
     def __init__(self, options):
-        # TODO make this one for loop
-        self.initial_stock_prices = np.array(
-            [option.initial_stock_price for option in options]
-        )
-        self.strike_prices = np.array([option.strike_price for option in options])
-        self.maturity_times = np.array([option.maturity_time for option in options])
-        self.risk_free_rates = np.array([option.risk_free_rate for option in options])
-        self.volatilities = np.array([option.volatility for option in options])
-        self.option_types = np.array([option.option_type for option in options])
-        self.option_start_dates = np.array([option.start_date for option in options])
+        self.initial_stock_prices = np.array([])
+        self.strike_prices = np.array([])
+        self.maturity_times = np.array([])
+        self.risk_free_rates = np.array([])
+        self.volatilities = np.array([])
+        self.option_types = np.array([])
+        self.option_start_dates = np.array([])
+
+        for option in options:
+            np.append(self.initial_stock_prices, option.initial_stock_price)
+            np.append(self.strike_prices, option.strike_price)
+            np.append(self.maturity_times, option.maturity_time)
+            np.append(self.risk_free_rates, option.risk_free_rate)
+            np.append(self.volatilities, option.volatility)
+            np.append(self.option_types, option.option_type)
+            np.append(self.option_start_dates, option.start_date)
 
         self.__options = options
 
