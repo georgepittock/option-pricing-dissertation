c46492aae1296095c8d391c9deb72ed4a11e355f
George Pittock <gpittock4@gmail.com>
2024-05-20 11:19:29 +0100

    fix pricer not adding options to list

--- pricers/base/base_pricer.py
+++ pricers/base/base_pricer.py
@@ -11,16 +11,22 @@ class BasePricer(ABC):
         self.risk_free_rates = np.array([])
         self.volatilities = np.array([])
         self.option_types = np.array([])
-        self.option_start_dates = np.array([])
+        self.option_start_dates = np.array([], dtype="datetime64")
 
         for option in options:
-            np.append(self.initial_stock_prices, option.initial_stock_price)
-            np.append(self.strike_prices, option.strike_price)
-            np.append(self.maturity_times, option.maturity_time)
-            np.append(self.risk_free_rates, option.risk_free_rate)
-            np.append(self.volatilities, option.volatility)
-            np.append(self.option_types, option.option_type)
-            np.append(self.option_start_dates, option.start_date)
+            self.initial_stock_prices = np.append(
+                self.initial_stock_prices, option.initial_stock_price
+            )
+            self.strike_prices = np.append(self.strike_prices, option.strike_price)
+            self.maturity_times = np.append(self.maturity_times, option.maturity_time)
+            self.risk_free_rates = np.append(
+                self.risk_free_rates, option.risk_free_rate
+            )
+            self.volatilities = np.append(self.volatilities, option.volatility)
+            self.option_types = np.append(self.option_types, option.option_type)
+            self.option_start_dates = np.append(
+                self.option_start_dates, option.start_date
+            )
 
         self.__options = options
 
